a4788882f8f7ca0657d142dafb33d9634185f17c
David Smith <davidsmi@microsoft.com>
2019-06-27 17:12:07 -0500

    install packages in user lib

--- install-packages.R
+++ install-packages.R
@@ -1,1 +1,5 @@
-install.packages("xgboost", repos="https://cloud.r-project.org")
+## ensure user library exists, or else R will attempt to unstall to /usr/local/lib
+if(!dir.exists(Sys.getenv("R_LIBS_USER"))) {
+ dir.create(Sys.getenv("R_LIBS_USER"), recursive = TRUE) }
+
+install.packages("xgboost", repos="https://cran.rstudio.com")
