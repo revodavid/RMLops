ded6e25c6e18e429b48f8c4b96c25c19c701ccc4
David Smith <davidsmi@microsoft.com>
2019-06-27 17:24:35 -0500

    install packages to default R system dir

--- install-packages.R
+++ install-packages.R
@@ -1,5 +1,6 @@
 ## ensure user library exists, or else R will attempt to unstall to /usr/local/lib
-if(!dir.exists(Sys.getenv("R_LIBS_USER"))) {
- dir.create(Sys.getenv("R_LIBS_USER"), recursive = TRUE) }
+#if(!dir.exists(Sys.getenv("R_LIBS_USER"))) {
+# dir.create(Sys.getenv("R_LIBS_USER"), recursive = TRUE) }
 
+# ensure /usr/local/lib/R/site-library is writeable first
 install.packages("xgboost", repos="https://cran.rstudio.com")
