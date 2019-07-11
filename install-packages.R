95e2d00f4f6e43517341175ce4315b713dc35fd5
David Smith <davidsmi@microsoft.com>
2019-07-11 09:28:19 +0200

    Check user lib

--- install-packages.R
+++ install-packages.R
@@ -1,6 +1,8 @@
-## ensure user library exists, or else R will attempt to unstall to /usr/local/lib
-#if(!dir.exists(Sys.getenv("R_LIBS_USER"))) {
-# dir.create(Sys.getenv("R_LIBS_USER"), recursive = TRUE) }
+## ensure user library exists, or else R will attempt to install to /usr/local/lib
+print(Sys.getenv("R_LIBS_USER"))
+
+if(!dir.exists(Sys.getenv("R_LIBS_USER"))) {
+ dir.create(Sys.getenv("R_LIBS_USER"), recursive = TRUE) }
 
 # ensure /usr/local/lib/R/site-library is writeable first
 install.packages("xgboost", repos="https://cran.rstudio.com")
