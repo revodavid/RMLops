ec25ef81ad945cc17903e4c9e234bd03496f8d75
David Smith <davidsmi@microsoft.com>
2019-07-11 09:36:13 +0200

    create library in wd

--- install-packages.R
+++ install-packages.R
@@ -2,8 +2,10 @@
 print(Sys.getenv("R_LIBS_USER"))
 print(getwd())
 
-if(!dir.exists(Sys.getenv("R_LIBS_USER"))) {
- dir.create(Sys.getenv("R_LIBS_USER"), recursive = TRUE) }
+dir.create("library")
+
+#if(!dir.exists(Sys.getenv("R_LIBS_USER"))) {
+# dir.create(Sys.getenv("R_LIBS_USER"), recursive = TRUE) }
 
 # ensure /usr/local/lib/R/site-library is writeable first
-install.packages("xgboost", repos="https://cran.rstudio.com")
+install.packages("xgboost", repos="https://cran.rstudio.com", lib="library")
