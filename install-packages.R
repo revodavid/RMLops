6f20dbcfd4ae4abaac9859a45d567d58eaf57adc
David Smith <davidsmi@microsoft.com>
2019-10-23 17:24:06 -0700

    Use fixed MRAN snapshot

--- install-packages.R
+++ install-packages.R
@@ -8,4 +8,5 @@ dir.create("library")
 # dir.create(Sys.getenv("R_LIBS_USER"), recursive = TRUE) }
 
 # ensure /usr/local/lib/R/site-library is writeable first
-install.packages("xgboost", repos="https://cran.rstudio.com", lib="library")
+#install.packages("xgboost", repos="https://cran.rstudio.com", lib="library")
+install.packages("xgboost", repos='https://mran.microsoft.com/snapshot/2019-09-20/', lib="library")
